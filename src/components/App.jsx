272f4efc21fcfbedaa5ee26bf61021fc95814821
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-09-01 19:55:37 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 // import { useEffect } from "react-dom";
 import Header from "./Header";
 import Main from "./Main";
@@ -8,22 +8,22 @@ import Footer from "./Footer";
 export const App = () => {
   
 
-  useEffect(() => {
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "HfLUXlTQb06iHBVUlFrCYPtznjSyE7pW");
+//   useEffect(() => {
+//     var myHeaders = new Headers();
+//     myHeaders.append("apikey", "HfLUXlTQb06iHBVUlFrCYPtznjSyE7pW");
     
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-      headers: myHeaders
-    };
+//     var requestOptions = {
+//       method: 'GET',
+//       redirect: 'follow',
+//       headers: myHeaders
+//     };
     
-    fetch("https://api.apilayer.com/fixer/convert?to=to&from=from&amount=amount", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-      // console.log(myHeaders);
-}, []);
+//     fetch("https://api.apilayer.com/fixer/convert?to=to&from=from&amount=amount", requestOptions)
+//       .then(response => response.text())
+//       .then(result => console.log(result))
+//       .catch(error => console.log('error', error));
+//       // console.log(myHeaders);
+// }, []);
 
   return (
     <div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function Main() {
+
+    // const [error, setError] = useState(null);
+    // const [isLoaded, setIsLoaded] = useState(false);
+    const [currencys, setCurrency] = useState([]);
+    // console.log(currency);
+
+    useEffect(() => {
+        // fetch("https://api.monobank.ua/bank/currency?date")
+        fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=5")
+        
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // setIsLoaded(true);
+          setCurrency(result);
+        },
+        // (error) => {
+        //   setIsLoaded(true);
+        //   setError(error);
+        // }
+      )
+    }, [])
+    console.log(currencys)
     return (
         <main>
             <section>
@@ -26,6 +49,7 @@ function Main() {
             </section>
         </main>
     );
+   
 }
 
 export default Main;
